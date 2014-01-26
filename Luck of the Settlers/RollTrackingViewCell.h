7cc991e341d8929f91eb4d23b79bb23487dc4dad
Max Bernardy <max.bernardy@gmail.com>
2014-01-26 13:15:26 -0500

    Added custom roll cells

---
+++ Luck of the Settlers/RollTrackingViewCell.h
@@ -0,0 +1,18 @@
+//
+//  RollTrackingViewCell.h
+//  Luck of the Settlers
+//
+//  Created by Max Bernardy on 1/26/14.
+//  Copyright (c) 2014 Max Bernardy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RollTrackingViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *rollValue;
+@property (weak, nonatomic) IBOutlet UIStepper *countStepper;
+
+@property (weak, nonatomic) IBOutlet UILabel *countValue;
+
+@end
---
+++ Luck of the Settlers/RollTrackingViewCell.m
@@ -0,0 +1,30 @@
+//
+//  RollTrackingViewCell.m
+//  Luck of the Settlers
+//
+//  Created by Max Bernardy on 1/26/14.
+//  Copyright (c) 2014 Max Bernardy. All rights reserved.
+//
+
+#import "RollTrackingViewCell.h"
+
+@implementation RollTrackingViewCell
+
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
